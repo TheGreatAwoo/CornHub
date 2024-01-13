36103631a8a3628a2fd0fd65d8e9665bcdeb494c
Mike <howard.itzer@proton.me>
2024-01-13 22:41:56 +1300

    Fix curios compatibility

--- src/main/java/com/corn/callofthecorn/Init/ClientEventSubscriber.java
+++ src/main/java/com/corn/callofthecorn/Init/ClientEventSubscriber.java
@@ -24,9 +24,6 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import top.theillusivec4.curios.api.CuriosApi;
-import top.theillusivec4.curios.api.SlotTypeMessage;
-import top.theillusivec4.curios.api.SlotTypePreset;
 
 @Mod.EventBusSubscriber(modid = Main.MODID,bus = Mod.EventBusSubscriber.Bus.MOD,value = Dist.CLIENT)
 public class ClientEventSubscriber {
@@ -46,15 +43,6 @@ public class ClientEventSubscriber {
 //        );
 //    }
 
-
-@SubscribeEvent
-public static void sendIMC(InterModEnqueueEvent event) {
-    InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.RING.getMessageBuilder().size(2).build());
-    InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.CHARM.getMessageBuilder().size(1).build());
-    InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.NECKLACE.getMessageBuilder().size(1).build());
-    InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.BELT.getMessageBuilder().size(1).build());
-    Main.LOGGER.info("Registered 4 Curio Types");
-}
 
 //    @SubscribeEvent
 //    public static void onRegisterBlocks(RegisterEvent.Register<Block> event) {
